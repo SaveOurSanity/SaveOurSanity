8106f1a0abda911f17520a49e5fe957132f4243a
Daniel Green <dan2552@gmail.com>
2015-09-24 20:04:55 +0100

    Fixes

--- app/models/environment.rb
+++ app/models/environment.rb
@@ -2,4 +2,13 @@ class Environment < ActiveRecord::Base
   validates :name, presence: true, allow_blank: false
   validates_format_of :url, :with => URI::regexp(%w(http https))
   has_many :deploys
+
+  def last_deployed_ruby_branch
+    deploys.last.try(:ruby_branch_name) || "n/a"
+  end
+
+  def last_deployed_php_branch
+    deploys.last.try(:php_branch_name) || "n/a"
+  end
+
 end
