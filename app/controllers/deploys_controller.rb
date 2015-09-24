bd310bc940d6b39ed69cbe2273d84a58a015bc48
Daniel Green <dan2552@gmail.com>
2015-09-24 21:05:43 +0100

    Deploys creation improvements

--- app/controllers/deploys_controller.rb
+++ app/controllers/deploys_controller.rb
@@ -1,26 +1,6 @@
 class DeploysController < ApplicationController
   before_action :set_deploy, only: [:show, :edit, :update, :destroy]
 
-  # GET /deploys
-  # GET /deploys.json
-  def index
-    @deploys = Deploy.all
-  end
-
-  # GET /deploys/1
-  # GET /deploys/1.json
-  def show
-  end
-
-  # GET /deploys/new
-  def new
-    @deploy = Deploy.new
-  end
-
-  # GET /deploys/1/edit
-  def edit
-  end
-
   # POST /deploys
   # POST /deploys.json
   def create
@@ -31,33 +11,9 @@ class DeploysController < ApplicationController
         format.html { redirect_to @deploy.environment, notice: 'Deploy was successfully created.' }
         format.json { render :show, status: :created, location: @deploy }
       else
-        format.html { render :new }
+        format.html { redirect_to @deploy.environment, notice: @deploy.errors.full_messages.first }
         format.json { render json: @deploy.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /deploys/1
-  # PATCH/PUT /deploys/1.json
-  def update
-    respond_to do |format|
-      if @deploy.update(deploy_params)
-        format.html { redirect_to @deploy, notice: 'Deploy was successfully updated.' }
-        format.json { render :show, status: :ok, location: @deploy }
-      else
-        format.html { render :edit }
-        format.json { render json: @deploy.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /deploys/1
-  # DELETE /deploys/1.json
-  def destroy
-    @deploy.destroy
-    respond_to do |format|
-      format.html { redirect_to deploys_url, notice: 'Deploy was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
